0b3977ac5854ea85b279a624d48a84892a7a7e6c
Sergey <sergey-707@mail.ru>
2024-03-26 21:27:21 +0400

    init project

---
+++ src/main/java/ru/grak/mediasofttask/MediasofttaskApplication.java
@@ -0,0 +1,13 @@
+package ru.grak.mediasofttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediasofttaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediasofttaskApplication.class, args);
+	}
+
+}
