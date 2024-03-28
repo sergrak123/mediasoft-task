e062abce7fc3ab66de6d2cdd6d7e1224a0e96f52
Sergey <sergey-707@mail.ru>
2024-03-29 01:21:53 +0400

    add javadoc for ProductService

--- src/main/java/ru/grak/mediasofttask/service/ProductService.java
+++ src/main/java/ru/grak/mediasofttask/service/ProductService.java
@@ -10,6 +10,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Service class for managing products.
+ */
 @Service
 public class ProductService {
 
@@ -20,17 +23,36 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    /**
+     * Retrieves all products.
+     *
+     * @return the list of all products
+     */
     public List<Product> getAllProducts() {
 
         return productRepository.findAll();
     }
 
+
+    /**
+     * Retrieves a product by ID.
+     *
+     * @param productId the product ID
+     * @return the product with the specified ID
+     * @throws ProductNotFoundException if the product with the given ID does not exist
+     */
     public Product getProductById(UUID productId) {
 
         return productRepository.findById(productId)
-                .orElseThrow(() -> new ProductNotFoundException("Продукт по id: {0} не найден", productId));
+                .orElseThrow(() -> new ProductNotFoundException("Product with id: {0} doesn't exist", productId));
     }
 
+    /**
+     * Creates a new product.
+     *
+     * @param product the product to create
+     * @return the created product
+     */
     public Product createProduct(Product product) {
 
         product.setCreationDateTime(LocalDateTime.now());
@@ -39,10 +61,18 @@ public class ProductService {
         return productRepository.save(product);
     }
 
+    /**
+     * Updates an existing product.
+     *
+     * @param productId       the ID of the product to update
+     * @param productDetails the updated product details
+     * @return the updated product
+     * @throws ProductNotFoundException if the product with the given ID does not exist
+     */
     public Product updateProduct(UUID productId, Product productDetails) {
 
         Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new ProductNotFoundException("Продукт по id: {0} не найден", productId));
+                .orElseThrow(() -> new ProductNotFoundException("Product with id: {0} doesn't exist", productId));
 
         //дату создания не трогаем, меняем только дату изменения количества (если количество изменилось)
         boolean wasChangedQuantity = !product.getQuantity().equals(productDetails.getQuantity());
@@ -64,10 +94,16 @@ public class ProductService {
         return productRepository.save(product);
     }
 
+    /**
+     * Deletes a product by ID.
+     *
+     * @param productId the ID of the product to delete
+     * @throws ProductNotFoundException if the product with the given ID does not exist
+     */
     public void deleteProduct(UUID productId) {
 
         Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new ProductNotFoundException("Продукт по id: {0} не найден", productId));
+                .orElseThrow(() -> new ProductNotFoundException("Product with id: {0} doesn't exist", productId));
 
         productRepository.delete(product);
     }
